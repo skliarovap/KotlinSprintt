2fbe7480b1215d0c39189437c5a3eb859240e4a8
skliarovap <iarkova9927@gmail.com>
2025-05-22 20:08:22 +1000

    KS-1-3 var/val?

---
+++ app/src/main/java/com/kotlinsprint/lesson_1/lesson1_task3.kt
@@ -0,0 +1,15 @@
+package com.kotlinsprint.lesson_1
+
+fun main() {
+    val year: Int = 1961
+    var hour: String = "09"
+    var minute: String = "07"
+
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour = "10"
+    minute = "55"
+    println("$hour:$minute")
+}
