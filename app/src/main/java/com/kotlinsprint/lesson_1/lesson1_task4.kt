11af61a13919904bf50f44f961e536b0ee4eeab8
skliarovap <iarkova9927@gmail.com>
2025-05-22 20:16:13 +1000

    KS-1-4 types

---
+++ app/src/main/java/com/kotlinsprint/lesson_1/lesson1_task4.kt
@@ -0,0 +1,10 @@
+package com.kotlinsprint.lesson_1
+
+fun main() {
+    val length: Long = 40868600000
+    val age: Byte = 27
+    val partOfDay: Float = 0.075F
+    val seconds: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
+}
