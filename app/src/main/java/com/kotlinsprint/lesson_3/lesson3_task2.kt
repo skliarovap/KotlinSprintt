09196e2221a20ecf5fa3be778c279e35ed0f7454
skliarovap <iarkova9927@gmail.com>
2025-05-23 18:39:05 +1000

    KS-3-2 married woman

---
+++ app/src/main/java/com/kotlinsprint/lesson_3/lesson3_task2.kt
@@ -0,0 +1,13 @@
+package com.kotlinsprint.lesson_3
+
+fun main() {
+    val fistName = "Татьяна"
+    var lastName = "Андреева"
+    val surname = "Сергеевна"
+    var years = 20
+    println("$lastName $fistName $surname, $years")
+
+    lastName = "Сидорова"
+    years = 22
+    println("$lastName $fistName $surname, $years")
+}
