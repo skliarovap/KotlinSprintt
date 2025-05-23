b67c11cb06b8b6e9073e6a5bedf16b6a4c7662c3
skliarovap <iarkova9927@gmail.com>
2025-05-23 18:29:15 +1000

    KS-3-1 greeting

---
+++ app/src/main/java/com/kotlinsprint/lesson_3/lesson3_task1.kt
@@ -0,0 +1,11 @@
+package com.kotlinsprint.lesson_3
+
+fun main() {
+    val name = "Полина"
+
+    var greetings = "Добрый день"
+    println("$greetings $name")
+
+    greetings = "Добрый вечер"
+    println("$greetings $name")
+}
