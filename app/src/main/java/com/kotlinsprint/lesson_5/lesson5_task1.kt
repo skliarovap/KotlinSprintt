b64980ae61dc4f08e365e291596a699c946312ab
skliarovap <iarkova9927@gmail.com>
2025-05-26 14:28:51 +1000

    KS-5-1 check

---
+++ app/src/main/java/com/kotlinsprint/lesson_5/lesson5_task1.kt
@@ -0,0 +1,11 @@
+package com.kotlinsprint.lesson_5
+
+fun main() {
+    println("10+10")
+    val result = 20
+
+    val answer = readln().toInt()
+
+    if (result == answer) println("Добро пожаловать!")
+    else println("Доступ запрещен.")
+}
